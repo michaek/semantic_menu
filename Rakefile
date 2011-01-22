d8e618085cc03ecf0f35862714fd19dbd331d479
Michael Hellein <michael@beardeds.com>
2011-01-22 12:51:27 -0500

    added gemspec and configuration

--- Rakefile
+++ Rakefile
@@ -1,18 +1,48 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 require 'rake'
+
+require 'jeweler'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "semantic_menu"
+  gem.homepage = "http://github.com/michaek/semantic_menu"
+  gem.license = "MIT"
+  gem.summary = %Q{A plugin to make rails menus easier to write}
+  gem.description = %Q{A plugin to make rails menus easier to write}
+  gem.email = ""
+  gem.authors = ["Daniel Haran"]
+  # Include your dependencies below. Runtime dependencies are required when using your gem,
+  # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
+  #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
+  #  gem.add_development_dependency 'rspec', '> 1.2.3'
+end
+Jeweler::RubygemsDotOrgTasks.new
+
 require 'rake/testtask'
-require 'rake/rdoctask'
-
-desc 'Default: run unit tests.'
-task :default => :test
-
-desc 'Test the semantic_menu plugin.'
-Rake::TestTask.new(:test) do |t|
-  t.libs << 'lib'
-  t.pattern = 'test/**/*_test.rb'
-  t.verbose = true
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
 end
 
-desc 'Generate documentation for the semantic_menu plugin.'
+require 'rcov/rcovtask'
+Rcov::RcovTask.new do |test|
+  test.libs << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+end
+
+task :default => :test
+
+require 'rake/rdoctask'
 Rake::RDocTask.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
   rdoc.title    = 'SemanticMenu'
--- lib/semantic_menu.rb
+++ lib/semantic_menu.rb
@@ -4,57 +4,84 @@ require 'active_support'
 
 class MenuItem
   include ActionView::Helpers::TagHelper,
-          ActionView::Helpers::UrlHelper
-  
+    ActionView::Helpers::UrlHelper
+
   attr_accessor :children, :link
   cattr_accessor :controller
-  
+
   def initialize(title, link, level, link_opts={})
     @title, @link, @level, @link_opts = title, link, level, link_opts
     @children = []
   end
-  
+
   def add(title, link, link_opts={}, &block)
-    returning(MenuItem.new(title, link, @level +1, link_opts)) do |adding|
+    MenuItem.new(title, link, @level +1, link_opts).tap do |adding|
       @children << adding
       yield adding if block_given?
     end
   end
-  
+
   def to_s
-    content_tag :li, link_to(@title, @link, @link_opts) + child_output, ({:class => 'active'} if active?)
+    content_tag :li, SemanticMenu::Util.html_safe(link_to(@title, @link, @link_opts) + child_output), ({:class => 'active'} if active?)
   end
-  
+
   def level_class
     "menu_level_#{@level}"
   end
-  
+
   def child_output
-    children.empty? ? '' : content_tag(:ul, @children.collect(&:to_s).join, :class => level_class)
+    children.empty? ? '' : content_tag(:ul, SemanticMenu::Util.html_safe(@children.collect(&:to_s).join), :class => level_class)
   end
-  
+
   def active?
     children.any?(&:active?) || on_current_page?
   end
-  
+
   def on_current_page?
-    @controller = @@controller # set it for current_page? defined in UrlHelper
     current_page?(@link)
+  end
+
+  # Provide a 'request' method to account for Rails 3's new current_page?-method
+  def request
+    @@controller.request
   end
 end
 
 class SemanticMenu < MenuItem
-  
+  # Adapted from Formtastic::Util, which was in turn
+  # Adapted from the rails3 compatibility shim in Haml 2.2
+  module Util
+    extend self
+    ## Rails XSS Safety
+
+    # Returns the given text, marked as being HTML-safe.
+    # With older versions of the Rails XSS-safety mechanism,
+    # this destructively modifies the HTML-safety of `text`.
+    #
+    # @param text [String]
+    # @return [String] `text`, marked as HTML-safe
+    def html_safe(text)
+      return text if text.nil?
+      return text.html_safe if defined?(ActiveSupport::SafeBuffer)
+      return text.html_safe!
+    end
+
+    def rails_safe_buffer_class
+      return ActionView::SafeBuffer if defined?(ActionView::SafeBuffer)
+      ActiveSupport::SafeBuffer
+    end
+  end
+
   def initialize(controller, opts={},&block)
-   @@controller = controller
+    @@controller = controller
     @opts       = {:class => 'menu'}.merge opts
     @level      = 0
     @children   = []
-    
+
     yield self if block_given?
   end
 
   def to_s
-    content_tag(:ul, @children.collect(&:to_s).join, @opts)
+    content_tag(:ul, SemanticMenu::Util.html_safe(@children.collect(&:to_s).join), @opts)
   end
 end
